e5e70227e473c2626e2e6289b3e2d7e2d6d43a26
Juan Pablo Calle <52135381+JuanCalle1606@users.noreply.github.com>
2020-10-13 17:06:50 -0500

    Aumentado de short a int

--- ConsoleFile.java
+++ ConsoleFile.java
@@ -43,7 +43,7 @@ public class ConsoleFile{
 	public static String[] arrayListToStrings(ArrayList<String> al)
 	{
 		String []out = new String[al.size()];
-		short count = 0;
+		int count = 0;
 		for (String token : al) {
 			out[count++] = token;
 			//System.out.print(token);
@@ -89,7 +89,7 @@ public class ConsoleFile{
 	public static String[] stringToArray(String str){
 		StringTokenizer st = new StringTokenizer(str,";");
 		String[] out = new String[st.countTokens()];
-		byte count = 0;
+		int count = 0;
 		while(st.hasMoreTokens()){
 			out[count++] = st.nextToken();
 			//System.out.print(token);
